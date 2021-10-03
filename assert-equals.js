ae5f87677e0d8bc0dfc143988177ea497fca4f98
Andreea <andreeailiescu.digital@gmail.com>
2021-10-03 15:35:17 +0100

    first push

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,37 @@
-function assertEquals(expect, actual) {}
+// function assertEquals(expect, actual) {
+// 	if (typeof expect == typeof actual) {
+// 		if (expect.length != actual.length) {
+// 			return `Expected array length ${expect.length} but found ${actual.length}`;
+// 		}
 
-module.exports = assertEquals
+// 		return 'no error';
+// 	} else if (expect.length != actual.length)
+// 		return `expected type of ${typeof expect} but found type of ${typeof actual} `;
+// }
+
+function assertEquals(expect, actual) {
+	if (typeof expect != typeof actual) {
+		return `Expected type of ${typeof expect} but found type of ${typeof actual} `;
+	} else if (expect !== actual) {
+		if (expect.constructor == Array) {
+			if (expect.length == actual.length) {
+				for (let i = 0; i <= expect.length; i++)
+					if (expect[i] != actual[i]) {
+						return `Expected  ${expect[i]} but found ${actual[i]} `;
+					}
+				return 'no error';
+			} else return `Expected array length ${expect.length} but found ${actual.length} `;
+		} else return `Expected  ${expect} but found ${actual} `;
+	} else {
+		return 'no error';
+	}
+}
+module.exports = assertEquals;
+console.log(assertEquals('abc', 'abc'));
+console.log(assertEquals('abcef', 'abc'));
+console.log(assertEquals(1, 1));
+console.log(assertEquals(1, 2));
+console.log(assertEquals(1, '1'));
+console.log(assertEquals(['a', 'b', 'c'], ['a', 'b', 'c']));
+console.log(assertEquals(['a', 'b'], ['a', 'b', 'c']));
+console.log(assertEquals(['a', 'b'], ['a', 'd']));
